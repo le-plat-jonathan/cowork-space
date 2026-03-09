d47a9234e7f6d39202345930936cf2088b938ae6
Ls_dev <110622285+LsDevFrance@users.noreply.github.com>
2026-03-09 14:33:37 +0100

    feat sidebar

---
+++ app/app/_navigation/dashboard-sidebar.tsx
@@ -0,0 +1,76 @@
+"use client";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarSeparator,
+} from "@/components/ui/sidebar";
+import { cn } from "@/lib/utils";
+import { CalendarIcon, HomeIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const firstSection = [
+  {
+    icon: HomeIcon,
+    label: "Accueil",
+    href: "/app",
+  },
+  {
+    icon: CalendarIcon,
+    label: "Reservations",
+    href: "/app/bookings",
+  },
+];
+export default function DashboardSidebar() {
+  const pathname = usePathname();
+  return (
+    <Sidebar>
+      <SidebarHeader className="text-sidebar-accent-foreground">
+        <Link href="/" className="flex items-center gap-2 px-2 pt-2">
+          <Image src="/logo.svg" alt="Logo" width={36} height={36} />
+          <p className="text-2xl font-semibold">Cowork</p>
+        </Link>
+      </SidebarHeader>
+      <div className="px-4 py-2">
+        <SidebarSeparator className="opacity-10 text-[#5D6B68] h-px" />
+      </div>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {firstSection.map((item) => (
+                <SidebarMenuItem key={item.href}>
+                  <SidebarMenuButton
+                    asChild
+                    className={cn(
+                      "h-10 hover:bg-linear-to-r/oklch border border-none hover:border-[#5D6B6]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
+                      pathname === item.href &&
+                        "bg-linear-to-r/oklch border-[#5D6B6]/10",
+                    )}
+                    isActive={pathname === item.href}
+                  >
+                    <Link href={item.href}>
+                      <item.icon className="size-5" />
+                      <span className="text-sm font-medium tracking-tight">
+                        {item.label}
+                      </span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter></SidebarFooter>
+    </Sidebar>
+  );
+}
