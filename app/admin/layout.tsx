be2a131a9abe5d9ff6fb6973d11a5c47e883ad30
Ls_dev <110622285+LsDevFrance@users.noreply.github.com>
2026-03-16 10:38:26 +0100

    add sidebar trigger

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,4 +1,8 @@
-import { SidebarProvider } from "@/components/ui/sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 import AdminSidebar from "./_navigation/admin-sidebar";
 
 export default async function RouteLayout({
@@ -9,9 +13,14 @@ export default async function RouteLayout({
   return (
     <SidebarProvider>
       <AdminSidebar />
-      <main className="flex flex-col h-screen w-screen bg-muted">
-        {children}
-      </main>
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
+      </SidebarInset>
     </SidebarProvider>
   );
 }
--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,4 +1,8 @@
-import { SidebarProvider } from "@/components/ui/sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 import DashboardSidebar from "./_navigation/dashboard-sidebar";
 
 export default async function RouteLayout({
@@ -9,10 +13,14 @@ export default async function RouteLayout({
   return (
     <SidebarProvider>
       <DashboardSidebar />
-      <main className="flex flex-col h-screen w-screen bg-muted overflow-y-auto">
-        {children}
-      </main>
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
+      </SidebarInset>
     </SidebarProvider>
   );
 }
-
